fb6e5972f0db4dccfc990e6a7d5b1cf7941a3b5a
youenn priser <youenn.priser@gmail.com>
2020-02-25 12:27:01 +0100

    Implemented the Task destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read
   get 'tasks', to: 'tasks#index'
+  # Create
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+  # Show
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
