51b47105117deb8ae8cff2e2f2f64866e9bd4a25
youenn priser <youenn.priser@gmail.com>
2020-02-25 12:09:15 +0100

    Implemented the Task new and create actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
